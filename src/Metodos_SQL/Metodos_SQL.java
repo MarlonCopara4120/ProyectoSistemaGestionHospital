1f1ef16477ad9a60c00a9c0aac278d2e95b0f98e
MarlonCopara4120 <mcopara4120@uta.edu.ec>
2022-01-09 00:44:21 -0500

    PSGH-6 Se agregó un método buscar nombre y  buscar al usuario registrado.

--- src/Metodos_SQL/Metodos_SQL.java
+++ src/Metodos_SQL/Metodos_SQL.java
@@ -2,11 +2,13 @@ package Metodos_SQL;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Metodos_SQL {
 
     public static PreparedStatement sentencia_preparada;
+    public static ResultSet resultado;
 
     public int guardar(String nombre, String apellidos, String servicio, String usuario, String contraseña) throws SQLException {
         int resultado = 0;
@@ -31,5 +33,51 @@ public class Metodos_SQL {
             System.out.println(e);
         }
         return resultado;
+    }
+
+    public static String buscarNombre(String usuario) {
+        String busqueda_nombre = null;
+        Connection conexion = null;
+
+        try {
+            conexion = ConexionBD.conectar();
+            String sentencia_buscar = ("SELECT Nombre,Apellido,Servicio FROM login WHERE Usuario = '" + usuario + "'");
+            sentencia_preparada = conexion.prepareStatement(sentencia_buscar);
+            resultado = sentencia_preparada.executeQuery();
+
+            if (resultado.next()) {
+                String nombre = resultado.getString("Nombre");
+                String apellidos = resultado.getString("Apellido");
+                busqueda_nombre = (nombre + "" + apellidos);
+
+                conexion.close();
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return busqueda_nombre;
+    }
+
+    public static String buscarUsuarioRegistrado(String usuario, String contraseña) {
+        String busqueda_usuario = null;
+        Connection conexion = null;
+
+        try {
+            conexion = ConexionBD.conectar();
+            String sentencia_buscar_usuario = ("SELECT Nombre,Apellido,Servicio,Usuario,Contraseña FROM login WHERE Usuario= '" + usuario + "' &&Contraseña='" + contraseña + "'");
+
+            sentencia_preparada = conexion.prepareStatement(sentencia_buscar_usuario);
+            resultado = sentencia_preparada.executeQuery();
+            if (resultado.next()) {
+                busqueda_usuario = "Usuario Encontrado";
+            } else {
+                busqueda_usuario = "Usuario no Encontrado";
+            }
+
+            conexion.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return busqueda_usuario;
     }
 }
